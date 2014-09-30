58c48404cc00ef9de432eacc294453f9ede4c5f0
Jax Sanders <jax.r.sanders@gmail.com>
2014-09-30 11:33:31 -0400

    cleaned up some typos, fixed variable defined in two different senses (joys of rewriting old code), added plotting, tested and it works

--- GS_analytic_upper_limits.py
+++ GS_analytic_upper_limits.py
@@ -2,12 +2,13 @@
 
 #GS_analytic_upper_limits - collects analytic upper limits and plots automagically.
 
-import re, math,os
-import matplotlib as plt 
+import re, math,os, getopt
+import matplotlib.pyplot as plt 
+from pylab import *
  
 def main(argv):
 
-	usage = "GS_analytic_upper_limits -s <start frequency> -e <end frequency> [-f <file location> -b <freq band size> -o <output filename> -d <output directory>]"
+	usage = "GS_analytic_upper_limits -s <start frequency> -e <end frequency> [-p (plot flag) -f <file location> -b <freq band size> -o <output filename> -d <output directory>]"
 
 	#load inputs/options
 	
@@ -17,9 +18,10 @@ def main(argv):
 	band = 0.1
 	outputDir = '.'
 	outFile = False
+	plotResults = False
 
 	try:
-		opts, args = getopt.getopt(argv, "hs:e:f:b:o:d:", ["help", "startFreq=", "endFreq=", "files=", "band=", "outputFile=", "outputDir="])
+		opts, args = getopt.getopt(argv, "hps:e:f:b:o:d:", ["help", "plot", "startFreq=", "endFreq=", "files=", "band=", "outputFile=", "outputDir="])
 	except getopt.GetoptError:
 		print usage
 		sys.exit(2)
@@ -40,6 +42,8 @@ def main(argv):
 			outFile = arg
 		elif opt in ("-d", "--outputDir"):
 			outputDir = arg
+		elif opt in ("-p", "--plot"):
+			plotResults = True
 
 	if not(outFile):
 		outFile = "Analytic_UL_" + str(startFreq) + "_" + str(endFreq) + ".dat"
@@ -55,24 +59,30 @@ def main(argv):
 	with open(outputDir + "/" + outFile, "w") as output:		
 
 		for step in xrange(0,freqSteps):
-		
 			freq0 = startFreq + step*band
 
 			filename = "UL_" + str(freq0) + "_band.txt"
 
 			with open(fileLocation + "/" + filename, "r") as f:
-				data = f.readlines()[-3;-2]
+				data = f.readlines()[-3:-2]
 	
 			m = re.search('(?<=h0=)\d.\d+e-\d+', data[0])
 
-			band = {}
-			band['freq'] = freq0
-			band['UL'] = float(m.group(0))
+			ULband = {}
+			ULband['freq'] = freq0
+			ULband['UL'] = float(m.group(0))
 	
-			output.write(str(band['freq']) + " " + str(band['UL'])+"\n"	
+			output.write(str(ULband['freq']) + " " + str(ULband['UL'])+"\n")	
 	
 			ULFreqs.append(freq0)
-			ULims.append(float(m.group(0))		
+			ULims.append(float(m.group(0)))		
+	
+
+	if plotResults:
+		plt.figure()
+		plt.plot(ULFreqs, ULims, 'ro')
+		plt.show()
+	
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
