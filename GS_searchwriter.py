417dec4a414b21e2780603895553455efd769747
Jax Sanders <jax.r.sanders@gmail.com>
2014-10-16 10:19:17 -0400

    Switched to config file basis and debugged.

--- GS_searchwriter.py
+++ GS_searchwriter.py
@@ -14,14 +14,14 @@
 # define variables to be loaded from config file
 
 from __future__ import division
-import sys, getopt, re, os, math
+import sys, getopt, re, os, math, ConfigParser
 import numpy as np
 from subprocess import call
 
 
 def main(argv):
 	
-	usage = "GS_searchwriter -s <start frequency> -e <end frequency> -c <config file> [-b <freq band> -o <output directory> -m <mismatch> -t <tau> -l <lower search bound> --subFiles <sub file location>]"
+	usage = "GS_searchwriter -s <start frequency> -e <end frequency> -c <config file> [-o <output directory> --subFiles <sub file location>]"
 
 	#load inputs/options
 	
@@ -32,17 +32,9 @@ def main(argv):
 	subFileLocation = "SubFiles"
 
 	
-	band = 0.1
-	tau = 200.0*365*24*3600
-	mismatch = 0.2
-	lowestFreq=50.0
-	resConstant = 0.83
-	TwoFThreshold = 40
-	startTime = 970840605
-	endTime = 971272605
 
 	try:
-		opts, args = getopt.getopt(argv, "hs:e:c:b:o:t:m:l:", ["help", "startFreq=", "endFreq=", "config=", "band=","outputDir=","tau=","mismatch=", "lowestFreq=", "subFiles="])
+		opts, args = getopt.getopt(argv, "hs:e:c:o:", ["help", "startFreq=", "endFreq=", "config=","outputDir=","subFiles="])
 	except getopt.GetoptError:
 		print usage
 		sys.exit(2)
@@ -56,17 +48,9 @@ def main(argv):
 		elif opt in ("-e", "--endFreq"):
 			endFreq = float(arg)
 		elif opt in ("-c", "--config"):
-			configFile = arg
-		elif opt in ("-b", "--band"):
-			band = float(arg)	
+			configFile = arg	
 		elif opt in ("-o", "--outputDir"):
 			outputDir = arg
-		elif opt in ("-t", "--tau"):
-			tau = float(arg)*365*24*3600
-		elif opt in ("-m", "--mismatch"):
-			mismatch = float(arg)
-		elif opt in ("-l", "--lowestFreq"):
-			lowestFreq = float(arg)
 		elif opt in ("--subFiles"):
 			subFileLocation = arg
 
@@ -76,37 +60,143 @@ def main(argv):
 		
 	#read from config file
 
-	configData = []
+	try:
+		config = ConfigParser.ConfigParser()
+		config.read(configFile)
+	except:
+		sys.stderr.write("Cannot import Config File " + configFile + " exiting...\n")
+		sys.exit(1)
 
-	with open(configFile, 'r') as f:
-		for line in f:
-			m = re.search('(?<==).+',line)
-			configData.append(m.group(0))
+	Vars = {}
 	
+	try:
+		Vars['sourceNumber'] = int(config.get("InjVars","Source"))
+	except:
+		sys.stderr.write("Cannot read sourceNumber\n")
+		sys.exit(1)
+	
+	try:
+		Vars['Alpha'] = float(config.get("InjVars","Alpha"))
+	except:	
+		sys.stderr.write("Cannot read Alpha\n")
+		sys.exit(1)
+		
+	try:
+		Vars['Delta'] = float(config.get("InjVars","Delta"))
+	except:
+		sys.stderr.write("Cannot read Delta\n")
+		sys.exit(1)
 
-	sourceNumber = configData[0]
-	Alpha = float(configData[1])
-	Delta = float(configData[2])
-	Semimajor = float(configData[3])
-	Semiminor = float(configData[4])
-	Semiangle = float(configData[5])
-	H1SFTlocation = configData[6]
-	L1SFTlocation = configData[7]
+	try:
+		Vars['Semimajor'] = float(config.get("InjVars","Semimajor"))
+	except:
+		sys.stderr.write("Cannot read Semimajor\n")
+		sys.exit(1)
+	
+	try:
+		Vars['Semiminor'] = float(config.get("InjVars","Semiminor"))
+	except:
+		sys.stderr.write("Cannot read Semiminor\n")
+		sys.exit(1)
+
+	try:
+		Vars['Semiangle'] = float(config.get("InjVars","Semiangle"))
+	except:
+		sys.stderr.write("Cannot read Semiangle\n")
+		sys.exit(1)
+
+	try:
+		Vars['resConstant'] = float(config.get("InjVars","resConstant"))
+	except:
+		sys.stderr.write("Cannot read resConstant\n")
+		sys.exit(1)
+
+	try:
+		Vars['Band'] = float(config.get("InjVars", "band"))
+		band = Vars['Band']
+	except:
+		sys.stderr.write("Cannot read band\n")
+		sys.exit(1)
+
+	try:
+		Vars['H1SFTlocation'] = config.get("InjVars","H1InputData")
+	except:
+		sys.stderr.write("Cannot read H1SFTlocation\n")
+		sys.exit(1)
+
+	try:
+		Vars['L1SFTlocation'] = config.get("InjVars","L1InputData")
+	except:
+		sys.stderr.write("Cannot read L1SFTlocation\n")
+		sys.exit(1)
+	
+	try:
+		Vars['Tau'] = float(config.get("InjVars","Age"))
+		Vars['Tau'] = Vars['Tau']*24*365*3600
+	except:
+		sys.stderr.write("Cannot read Tau\n")
+		sys.exit(1)
+	
+	try:
+		Vars['m'] = float(config.get("InjVars","Mismatch"))
+	except:
+		sys.stderr.write("Cannot read m\n")
+		sys.exit(1)
+	
+	try:
+		Vars['lowestFreq'] = float(config.get("InjVars","lowestF"))
+	except:
+		sys.stderr.write("Cannot read lowestFreq\n")
+		sys.exit(1)
+
+	try:
+		Vars['startTime'] = float(config.get("InjVars","StartTime"))
+	except:
+		sys.stderr.write("Cannot read startTime\n")
+		sys.exit(1)
+
+	try:
+		Vars['searchDuration'] = float(config.get("InjVars","SearchTime"))
+	except:
+		sys.stderr.write("Cannot read searchDuration\n")
+		sys.exit(1)
+
+	try:
+		Vars['2FThresh'] = float(config.get("InjVars", "2F"))
+	except:
+		sys.stderr.write("Cannot read 2FThresh\n")
+		sys.exit(1)
+
+	try:	
+		Vars['EphemEarth'] = config.get("InjVars","EphemEarth")
+	except:
+		sys.stderr.write("Cannot read EphemEarth\n")
+		sys.exit(1)
+		
+	try:
+		Vars['EphemSun'] = config.get("InjVars","EphemSun")
+	except:
+		sys.stderr.write("Cannot read EphemSun\n")
+		sys.exit(1)		
+
+	Vars['endTime'] = Vars['startTime'] + Vars['searchDuration']
+
+
 
 	freqRange = endFreq-startFreq
 	freqSteps = int(round(freqRange/band))
 
-	dagName = "GS_"+str(sourceNumber)+"_"+str(startFreq)+".dag"
+	dagName = "GS_"+str(Vars['sourceNumber'])+"_"+str(startFreq)+".dag"
 
 	if os.path.isfile(dagName):
 		os.remove(dagName)
 
 	if not(outputDir):
-		outputDir = "GS_"+str(sourceNumber)
+		outputDir = "GS_"+str(Vars['sourceNumber'])
 	if not(os.path.isdir(outputDir)):
 		os.makedirs(outputDir)
 
-	outputLocation = outputDir+"/"+"GS_"+str(sourceNumber)+"_"+str(startFreq)	
+	outputLocation = outputDir+"/"+"GS_"+str(Vars['sourceNumber'])+"_"+str(startFreq)	
 	dataDir = outputLocation+"/Data"	
 
 	if not(os.path.isdir(outputLocation)):
@@ -118,7 +208,7 @@ def main(argv):
 				
 		freq = startFreq + x*band
 		
-		dataLocation = str(dataDir)+"/GS_"+str(sourceNumber)+"_"+str(freq)
+		dataLocation = str(dataDir)+"/GS_"+str(Vars['sourceNumber'])+"_"+str(freq)
 
 		if not(os.path.isdir(dataLocation)):
 			os.makedirs(dataLocation)
@@ -128,19 +218,19 @@ def main(argv):
 
 		#produce data files for searching using ConverttoSFTv2
 
-		#call(["lalapps_ConvertToSFTv2", "--inputSFTs="+H1SFTlocation, "--outputDir="+dataLocation, "--fmin="+str(minFreq), "--fmax="+str(maxFreq)])
-		#call(["lalapps_ConvertToSFTv2", "--inputSFTs="+L1SFTlocation, "--outputDir="+dataLocation, "--fmin="+str(minFreq), "--fmax="+str(maxFreq)])
+		#call(["lalapps_ConvertToSFTv2", "--inputSFTs="+Vars['H1SFTlocation'], "--outputDir="+dataLocation, "--fmin="+str(minFreq), "--fmax="+str(maxFreq)])
+		#call(["lalapps_ConvertToSFTv2", "--inputSFTs="+Vars['L1SFTlocation'], "--outputDir="+dataLocation, "--fmin="+str(minFreq), "--fmax="+str(maxFreq)])
 
 		
 		#calculate spindown parameters
 				
-		f1dot = -(freq+band)/tau
-		f1dotBand = (6*(freq+band)-lowestFreq)/(6*tau)
+		f1dot = -(freq+band)/Vars['Tau']
+		f1dotBand = (6*(freq+band)-Vars['lowestFreq'])/(6*Vars['Tau'])
 		
-		f2dot = 7*f1dot**2/lowestFreq
-		f2dotBand = (126*(freq+band)**3 - lowestFreq**3)/(18*tau**2*(freq+band)*lowestFreq)
+		f2dot = 7*f1dot**2/Vars['lowestFreq']
+		f2dotBand = (126*(freq+band)**3 - Vars['lowestFreq']**3)/(18*Vars['Tau']**2*(freq+band)*Vars['lowestFreq'])
 
-		resolution = resConstant/freq
+		resolution = Vars['resConstant']/freq
 	
 		count = 1
 		step = 1
@@ -149,25 +239,25 @@ def main(argv):
 		AlphaList = []
 		DeltaList = []
 
-		AlphaList.append(Alpha)
-		DeltaList.append(Delta)
+		AlphaList.append(Vars['Alpha'])
+		DeltaList.append(Vars['Delta'])
 
 		while (count > 0):
 			count = 0
 			for x in range(0,j):
 				t = 2.0*math.pi/float(j)*float(x)
-				alpha_temp = Alpha + math.cos(Semiangle)*resolution*step*math.cos(t) - math.sin(Semiangle)*resolution*step*math.sin(t)
-				delta_temp = Delta + math.sin(Semiangle)*resolution*step*math.cos(t) + math.cos(Semiangle)*resolution*step*math.sin(t)
+				alpha_temp = Vars['Alpha'] + math.cos(Vars['Semiangle'])*resolution*step*math.cos(t) - math.sin(Vars['Semiangle'])*resolution*step*math.sin(t)
+				delta_temp = Vars['Delta'] + math.sin(Vars['Semiangle'])*resolution*step*math.cos(t) + math.cos(Vars['Semiangle'])*resolution*step*math.sin(t)
 
-				if isInErrorEllipse(Alpha, Delta, Semimajor, Semiminor, Semiangle, alpha_temp, delta_temp):
-					AlphaList.append(Alpha)
-					DeltaList.append(Delta)
+				if isInErrorEllipse(Vars['Alpha'], Vars['Delta'], Vars['Semimajor'], Vars['Semiminor'], Vars['Semiangle'], alpha_temp, delta_temp):
+					AlphaList.append(alpha_temp)
+					DeltaList.append(delta_temp)
 					count = count + 1
 			j = 2*j
 
 		#write SUB file
 
-		subFileName = subFileLocation+"/GS_"+str(sourceNumber)+"_"+str(freq)+".sub"
+		subFileName = subFileLocation+"/GS_"+str(Vars['sourceNumber'])+"_"+str(freq)+".sub"
 
 		with open(subFileName,"w") as f:
 			f.write("universe=vanilla\n")
@@ -182,10 +272,10 @@ def main(argv):
 		#write DAG
 		with open(dagName, "a") as f:
 			for x in range(0, len(AlphaList)):
-				jobName = "GS_"+str(sourceNumber)+"_"+str(freq)+"_"+str(x)
+				jobName = "GS_"+str(Vars['sourceNumber'])+"_"+str(freq)+"_"+str(x)
 				f.write("JOB "+ jobName + " " + subFileName + "\n")
 				f.write("RETRY " + jobName + " 0\n")
-				f.write("VARS " + jobName + ' argList=" --Alpha=' + str(AlphaList[x]) + ' --Delta=' + str(DeltaList[x]) + ' --Freq=' + str(freq) + ' --f1dot=' + str(f1dot) + ' --f2dot=' + str(f2dot) + ' --f1dotBand=' + str(f1dotBand) + ' --f2dotBand=' + str(f2dotBand) + ' --DataFiles=' + dataLocation + '/*.sft --TwoFthreshold=' + str(TwoFThreshold) + ' --NumCandidatesToKeep=100 --gridType=8 --outputFstat=' + outputLocation + '/GammaSearch_' + str(freq) + '_' + str(x) + '.dat --outputFstatHist=' + outputLocation + '/GammaHist_' + str(freq) + '_' + str(x) + '.dat --outputLoudest=' + outputLocation + '/GammaLoud_' + str(freq) + '_' + str(x) + '.dat --outputLogfile=' + outputLocation + '/CFSlog.txt --refTime=' + str(startTime) + ' --minStartTime=' + str(startTime) + ' --maxEndTime=' + str(endTime) + ' --outputSingleFstats=TRUE --metricMismatch=' + str(mismatch) + ' --dFreq=1e-6 --useResamp=TRUE --ephemEarth=/mnt/qfs2/jaclyn.sanders/earth00-19-DE405.dat.gz --ephemSun=/mnt/qfs2/jaclyn.sanders/sun00-19-DE405.dat.gz"\n')
+				f.write("VARS " + jobName + ' argList=" --Alpha=' + str(AlphaList[x]) + ' --Delta=' + str(DeltaList[x]) + ' --Freq=' + str(freq) + ' --f1dot=' + str(f1dot) + ' --f2dot=' + str(f2dot) + ' --f1dotBand=' + str(f1dotBand) + ' --f2dotBand=' + str(f2dotBand) + ' --DataFiles=' + dataLocation + '/*.sft --TwoFthreshold=' + str(Vars['2FThresh']) + ' --NumCandidatesToKeep=100 --gridType=8 --outputFstat=' + outputLocation + '/GammaSearch_' + str(freq) + '_' + str(x) + '.dat --outputFstatHist=' + outputLocation + '/GammaHist_' + str(freq) + '_' + str(x) + '.dat --outputLoudest=' + outputLocation + '/GammaLoud_' + str(freq) + '_' + str(x) + '.dat --outputLogfile=' + outputLocation + '/CFSlog.txt --refTime=' + str(Vars['startTime']) + ' --minStartTime=' + str(Vars['startTime']) + ' --maxEndTime=' + str(Vars['endTime']) + ' --outputSingleFstats=TRUE --metricMismatch=' + str(Vars['m']) + ' --dFreq=1e-6 --useResamp=TRUE --ephemEarth='+ str(Vars['EphemEarth']) + ' --ephemSun='+str(Vars['EphemSun'])+'"\n')
 				f.write("\n")
 
 	
